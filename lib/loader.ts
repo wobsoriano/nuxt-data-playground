03073fe42a6bf4dc4e531dd1abac59c7409e2e59
wobsoriano <sorianorobertc@gmail.com>
2022-11-17 23:43:58 -0800

    add cache key

--- lib/loader.ts
+++ lib/loader.ts
@@ -1,5 +1,6 @@
 export async function useServerData<T>(fn: () => Promise<T>) {
-  const data = useState<T | null>(() => null)
+  const route = useRoute()
+  const data = useState<T | null>(`data-${route.path}`, () => null)
   
   if (process.server) {
     data.value = await fn()
